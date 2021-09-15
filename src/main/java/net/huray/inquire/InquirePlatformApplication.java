5005ce905819808fb357356588058ef563600297
choru90 <jkc1619@gmail.com>
2021-09-15 23:09:11 +0900

    초기 설정

---
+++ src/main/java/net/huray/inquire/InquirePlatformApplication.java
@@ -0,0 +1,13 @@
+package net.huray.inquire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquirePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InquirePlatformApplication.class, args);
+    }
+
+}
